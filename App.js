f299b8e91739274778f0f2f2393254631ed4a4dd
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2022-02-21 13:48:18 +0100

    use of states and how to detect them.

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
+import React, {useState} from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [todoItem, setTodoItem] = useState("")
+  const [todoList, setTodoList] = useState([])
+
+
+  const addTodoList = () => {
+    setTodoList([...todoList, todoItem])
+    setTodoItem("")
+  }
   return (
     <View style={styles.container}>
       <View>
         {/* <Text>To do App!</Text> */}
         <TextInput
         style={styles.textInput} 
-        placeholder="Write a to do."/> 
+        placeholder="Write a to do."
+        onChangeText={text => setTodoItem(text)}
+        value={todoItem}
+        /> 
         <Button
           title="Add To do"
-          onPress={() => console.log("hello")}
+          onPress={addTodoList}
         />
       </View>
-      <View>
-        <Text>List of To do!</Text>
-      </View>
+      <ScrollView>
+        {
+          todoList.map((todo, index) => <View key={index} style={styles.todoItem}><Text>{todo}</Text></View>)
+        }
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -31,5 +45,13 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     marginBottom: 10
+  },
+  todoItem: {
+    marginTop: 10,
+    padding: 20,
+    backgroundColor: '#f2f2f2',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray'
   }
 });
